8f4c83d5ec0aea1f647a3133aa855a58e3288559
HGoscenski <hgoscenski@HGoscenski-Mac.local>
2016-05-11 23:47:45 -0500

    Removed Extra Crap

--- Perspectives Website Data Parser.py
+++ Perspectives Website Data Parser.py
@@ -1,8 +1,6 @@
 import json
-import time
 import xlwt
 from pathlib import Path
-from pprint import pprint
 n = 1
 def main():
     wb = xlwt.Workbook()
@@ -13,31 +11,14 @@ def main():
 
     jsonDump = []
     p = Path(str(input('Input directory of JSONDumps: ')))
-    # p = Path('/Users/hgoscenski/Perspectives/JSONDUMPS/JSON-03-09-22-07/')
     for child in p.iterdir():
-        # print(child)
         child = str(child)
         jsonDump.append(child)
-    # print(jsonDump)
 
-    # def collect():
-    #     Running = 1
-    #     while Running == 1:
-    #         temp = str(input('Enter file path to next JSON DUMP for this week, or if there are no more JSONDumps type "done": '))
-    #         if temp == 'done':
-    #             Running -= 1
-    #         elif temp == '':
-    #             print('Nothing was entered')
-    #         else:
-    #             jsonDump.append(temp)
-    #     print(jsonDump)
-    # collect()
     def addToXLS(data):
         global n
         for x in data['hits']:
-            # pprint(x)
             date = x["accessOnShort"][0:5:1] + '/2016'
-            # print(date)
             ws.write(n,0,date)
             ws.write(n,1,x["ipAddress"])
             ws.write(n,2,x["targetName"])
@@ -48,8 +29,6 @@ def main():
             data = json.load(datafile)
             addToXLS(data)
 
-    ws.write(0,3,(time.strftime("%m:%d:%Y")))
-    T = (time.strftime("%d/%m/%Y"))
     wb.save('/Users/hgoscenski/Desktop/WebsiteStats.xls')
     print('Saved XLS file to desktop! Have a great day!')
 main()
